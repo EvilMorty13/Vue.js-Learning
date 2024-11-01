d4ab00df308b7815904ed8ea86de3c279097a52a
Avik Singha <102418811+EvilMorty13@users.noreply.github.com>
2024-11-01 19:23:29 +0600

    Reaction timer game

---
+++ reaction-timer/src/components/Results.vue
@@ -0,0 +1,27 @@
+<template>
+  <p>Reaction Time {{score}} ms</p>
+  <p>Rank : {{calculateRank}}</p>
+</template>
+
+<script>
+export default {
+    props:['score'],
+    computed:{
+        calculateRank(){
+            if(this.score<250){
+                return 'Ninja'
+            }
+            else if(this.score<1000){
+                return 'Shinobi'
+            }
+            else{
+                return 'Sloth'
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
