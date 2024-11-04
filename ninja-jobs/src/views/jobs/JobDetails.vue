7b33c75dcb45f7a30f61bc3d567df9a6cb73340f
Avik Singha <102418811+EvilMorty13@users.noreply.github.com>
2024-11-04 15:44:07 +0600

    Vue router

---
+++ ninja-jobs/src/views/jobs/JobDetails.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Job detail page</h1>
+  <p>Job id is {{id}}</p>
+</template>
+
+<script>
+export default {
+    props:['id'],
+    data(){
+    }
+}
+</script>
+
+<style>
+
+</style>
